e163f4eaf5c3dd78af33f9a16ebd46d5c77ecb87
Maarten van Gompel <proycon@anaproy.nl>
2020-10-01 13:18:08 +0200

    don't output logo: null

--- contrib/harvest/clst-radboud.sh
+++ contrib/harvest/clst-radboud.sh
@@ -18,25 +18,25 @@ fi
 source clst-radboud-env/bin/activate
 
 #install the software  (no-op if already installed/up to date)
-pip install -U git+https://github.com/proycon/clam2switchboard.git
+pip install --no-cache-dir -U git+https://github.com/proycon/clam2switchboard.git
 
 cd $ROOT/tools
 
-clam2switchboard --url https://webservices.cls.ru.nl/frog -l nld --task "NLP suite for Dutch" #I don't like this task's title, Frog supports multiple tasks (see https://github.com/clarin-eric/switchboard/issues/67 and https://github.com/clarin-eric/switchboard-tool-registry/issues/5)
-clam2switchboard --url https://webservices.cls.ru.nl/ucto --task "Tokenisation"
-clam2switchboard --url https://webservices.cls.ru.nl/piereling --task "Data Conversion"
-clam2switchboard --url https://webservices.cls.ru.nl/alpino -l nld --task "Dependency Parsing"
-clam2switchboard --url https://webservices.cls.ru.nl/colibricore --task "N-Gramming"
-clam2switchboard --url https://webservices.cls.ru.nl/eng_ASR -l eng --task "Speech Recognition"
-clam2switchboard --url https://webservices.cls.ru.nl/oralhistory -l nld --task "Speech Recognition"
-clam2switchboard --url https://webservices.cls.ru.nl/spreek2schrijf -l nld --task "Speech Recognition"
-clam2switchboard --url https://webservices.cls.ru.nl/forcedalignment2 -l nld --task "Speech Recognition"
-clam2switchboard --url https://webservices.cls.ru.nl/glem -l grc --task "Lemmatization"
-clam2switchboard --url https://webservices.cls.ru.nl/g2pservice  --task "Grapheme to Phoneme Conversion"
-clam2switchboard --url https://webservices.cls.ru.nl/t2t -l rus,ukr,ara  --task "Transliteration"
-clam2switchboard --url https://webservices.cls.ru.nl/valkuil -l nld --task "Spelling correction"
-clam2switchboard --url https://webservices.cls.ru.nl/tscan -l nld --task "Text analytics"
-clam2switchboard --url https://webservices.cls.ru.nl/piccl --langparam lang --task "Text Normalisation" #not enough, needs multiple tasks
+clam2switchboard --ignore --url https://webservices.cls.ru.nl/frog -l nld --task "NLP suite for Dutch" #I don't like this task's title, Frog supports multiple tasks (see https://github.com/clarin-eric/switchboard/issues/67 and https://github.com/clarin-eric/switchboard-tool-registry/issues/5)
+clam2switchboard --ignore --url https://webservices.cls.ru.nl/ucto --task "Tokenisation"
+clam2switchboard --ignore --url https://webservices.cls.ru.nl/piereling --task "Data Conversion"
+clam2switchboard --ignore --url https://webservices.cls.ru.nl/alpino -l nld --task "Dependency Parsing"
+clam2switchboard --ignore --url https://webservices.cls.ru.nl/colibricore --task "N-Gramming"
+clam2switchboard --ignore --url https://webservices.cls.ru.nl/eng_ASR -l eng --task "Speech Recognition"
+clam2switchboard --ignore --url https://webservices.cls.ru.nl/oralhistory -l nld --task "Speech Recognition"
+clam2switchboard --ignore --url https://webservices.cls.ru.nl/spreek2schrijf -l nld --task "Speech Recognition"
+clam2switchboard --ignore --url https://webservices.cls.ru.nl/forcedalignment2 -l nld --task "Speech Recognition"
+clam2switchboard --ignore --url https://webservices.cls.ru.nl/glem -l grc --task "Lemmatization"
+clam2switchboard --ignore --url https://webservices.cls.ru.nl/g2pservice  --task "Grapheme to Phoneme Conversion"
+clam2switchboard --ignore --url https://webservices.cls.ru.nl/t2t -l rus,ukr,ara  --task "Transliteration"
+clam2switchboard --ignore --url https://webservices.cls.ru.nl/valkuil -l nld --task "Spelling correction"
+clam2switchboard --ignore --url https://webservices.cls.ru.nl/tscan -l nld --task "Text analytics"
+clam2switchboard --ignore --url https://webservices.cls.ru.nl/piccl --langparam lang --task "Text Normalisation" #not enough, needs multiple tasks
 
 
 
